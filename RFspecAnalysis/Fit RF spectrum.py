ecd0002fa861ac6e7cfbd71c58c8fa40e786ed0a
insommer <insommer@gmail.com>
2025-12-11 11:02:52 -0500

    daily changes

--- RFspecAnalysis/Fit RF spectrum.py
+++ RFspecAnalysis/Fit RF spectrum.py
@@ -7,19 +7,24 @@ import SpectrumAnalysisCode
 
 plt.close('all')
 
-dataRootFolder = r'C:\Users\wmmax\Documents\Lehigh\Sommer Group\Experiment Data'
+dataRootFolder = r'C:\Users\insommer\Lehigh University Dropbox\Ariel Sommer\Sommer Lab Shared\Data'
 
-date = '9/17/2025'
-# date = '12/2/2025'
+# date = '9/17/2025'
+date = '12/3/2025'
 
 data_folder = [
     
     # r'cMOT thermo'
     # r'RF after D1 cam bias 0.25 A track higher resonance',
     # r'RF after D1 cam bias 0.25 A track lower resonance',
-    r'RF after D1 cam bias 0.25 A scan about 228.2 MHz',
-    r'RF after D1 cam bias 0.25 A scan about 228.2 MHz_1',
+    # r'RF after D1 cam bias 0.25 A scan about 228.2 MHz',
+    # r'RF after D1 cam bias 0.25 A scan about 228.2 MHz_1',
     # r'D1_RFscan_noRamp_Vert1.33A_ScanZS_0_0.3A_cam0.11A'
+    'D1_RFscan_noRamp_Vert1.33A_ZS0.38A_Cam0.15A',
+    'D1_RFscan_noRamp_Vert1.33A_ZS0.38A_Cam0.15A_1',
+    'D1_RFscan_noRamp_Vert1.33A_ZS0.38A_Cam0.15A_2',
+    'D1_RFscan_noRamp_Vert1.33A_ZS0.38A_Cam0.15A_3',
+    
 
     ]
 ####################################
@@ -47,7 +52,8 @@ results = pd.concat(dfs, ignore_index=True)
 
 plt.rcParams['font.size'] = 14
 
-peak_sep_MHz = 0.15
-sigma_guess = 0.05
+peak_sep_MHz = 0.1
+sigma_guess = 0.5
+
 
 stats = SpectrumAnalysisCode.FitRFspectrum(results, peak_sep_MHz, sigma_guess)
--- RFspecAnalysis/SpectrumAnalysisCode.py
+++ RFspecAnalysis/SpectrumAnalysisCode.py
@@ -27,7 +27,7 @@ def FitRFspectrum(dataFrame, peak_sep_MHz=0.15, sigma_guess=0.05, doPlot=True):
     Freq = df['RF_FRQ_MHz'].values
     Response = df['XatomNumber'].interpolate().values # in case there are nan values
 
-    ResponseSmoothed = savgol_filter(Response, window_length=7, polyorder=2)
+    ResponseSmoothed = savgol_filter(Response, window_length=4, polyorder=2)
 
     # Peak detection
     freq_step = np.mean(np.diff(Freq))
